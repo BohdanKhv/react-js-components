41b0377cb9ada547f0064dae870aa57c309f5538
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-05-10 16:46:30 -0400

    image

---
+++ image/Image.jsx
@@ -0,0 +1,48 @@
+import { useState } from 'react';
+import { closeIcon } from '../../constance/icons';
+import './styles/Image.css';
+
+const Image = ({image, alt, classList}) => {
+    const [showImage, setShowImage] = useState(false);
+
+    const onClickOutside = (e) => {
+        if (e.target.classList.contains('image-overlay') || e.target.classList.contains('image-wrapper')) {
+            setShowImage(false);
+        }
+    }
+
+    return (
+        <>
+        {showImage && (
+            <div 
+                className="image-overlay"
+                onClick={onClickOutside}
+            >
+                <div className="close position-absolute">
+                    <div 
+                        className="btn-icon"
+                        onClick={() => setShowImage(false)}
+                        title="Close"
+                    >
+                        {closeIcon}
+                    </div>
+                </div>
+                <div className="image-wrapper">
+                    <img
+                        src={image}
+                        alt={alt}
+                    />
+                </div>
+            </div>
+        )}
+        <img
+            src={image}
+            alt={alt}
+            className={`image-btn ${classList}`}
+            onClick={() => setShowImage(!showImage)}
+        />
+        </>
+    )
+}
+
+export default Image
