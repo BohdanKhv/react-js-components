6c9347eb8509f0e3d391b1f9bc0dda304e30212e
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-05-11 21:59:18 -0400

    firebase image upload

---
+++ firebase_fileupload/UploadImage.jsx
@@ -0,0 +1,68 @@
+import { useState, useRef } from 'react';
+import { storage } from '../../firebase';
+import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
+import './styles/UploadImage.css';
+
+const UploadImage = ({ image, label, folder, fileName, containerClass, updateData, imageClass }) => {
+    const [progress, setProgress] = useState(0);
+    const inputRef = useRef(null);
+
+    const uploadImageToFirebase = (e) => {
+        const file = e.target.files[0];
+        const storageRef = ref(storage, `${folder}/${fileName}`);
+        const metadata = {
+            contentType: 'image/jpeg',
+        };
+
+        const uploadTask  = uploadBytesResumable(storageRef, file, metadata);
+
+        uploadTask.on('state_changed', 
+        (snapshot) => {
+          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+            setProgress(progress);
+        }, 
+        (error) => {
+            console.log(error);
+        }, 
+        () => {
+            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                updateData(label, downloadURL);
+            });
+        }
+    );
+    }
+
+    return (
+        <div
+            className={`upload-image-container ${containerClass}`}
+            onClick={() => {
+                inputRef.current.click();
+            }}
+        >
+            <div 
+                className={`upload-image-progress ${containerClass}`}
+                style={{
+                    height: progress + '%'
+                }}
+            />
+            {image && (
+                <img
+                    src={image} 
+                    alt={label}
+                    className={`upload-image ${imageClass}`}
+                />
+            )}
+            <input
+                type="file"
+                accept="image/*"
+                className="upload-image-input"
+                ref={inputRef}
+                onChange={(e) => {
+                    uploadImageToFirebase(e);
+                }}
+            />
+        </div>
+    )
+}
+
+export default UploadImage
