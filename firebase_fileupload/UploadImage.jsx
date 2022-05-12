8b7b52172c041dcde7ee06846a2d801bcf47bbb3
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-05-12 19:39:25 -0400

    filesize

--- firebase_fileupload/UploadImage.jsx
+++ firebase_fileupload/UploadImage.jsx
@@ -4,39 +4,45 @@ import { Image } from '../';
 import { ref, uploadBytesResumable, getDownloadURL } from "firebase/storage";
 import './styles/UploadImage.css';
 
-const UploadImage = ({ image, label, folder, fileName, containerClass, updateData, imageClass }) => {
+const UploadImage = ({ image, label, folder, fileName, containerClass, updateData, imageClass, maxFileSize }) => {
     const [progress, setProgress] = useState(0);
+    const [err, setErr] = useState('');
     const inputRef = useRef(null);
 
     const uploadImageToFirebase = (e) => {
         const file = e.target.files[0];
-        const storageRef = ref(storage, `${folder}/${fileName}`);
-        const metadata = {
-            contentType: 'image/jpeg',
-        };
+        if(file.size > 	maxFileSize) {
+            setErr('File size is too big. Max size is 3MB');
+        } else {
+            const storageRef = ref(storage, `${folder}/${fileName}`);
+            const metadata = {
+                contentType: 'image/jpeg',
+            };
 
-        const uploadTask  = uploadBytesResumable(storageRef, file, metadata);
+            const uploadTask  = uploadBytesResumable(storageRef, file, metadata);
 
-        uploadTask.on('state_changed', 
-        (snapshot) => {
-          const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
-            setProgress(progress);
-        }, 
-        (error) => {
-            console.log(error);
-        }, 
-        () => {
-            getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
-                updateData(label, downloadURL);
-            });
+            uploadTask.on('state_changed', 
+                (snapshot) => {
+                const progress = (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+                    setProgress(progress);
+                }, 
+                (error) => {
+                    console.log(error);
+                }, 
+                () => {
+                    getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+                        updateData(label, downloadURL);
+                    });
+                }
+            );
         }
-    );
     }
 
     return (
         <div
             className={`upload-image-container ${containerClass}`}
             onClick={() => {
+                setErr('');
                 inputRef.current.click();
             }}
         >
@@ -62,6 +68,11 @@ const UploadImage = ({ image, label, folder, fileName, containerClass, updateDat
                     uploadImageToFirebase(e);
                 }}
             />
+            {err && (
+                <div className="text-danger">
+                    {err}
+                </div>
+            )}
         </div>
     )
 }
