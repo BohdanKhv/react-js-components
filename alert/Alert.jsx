77df45e53fd104431d700dfc6c9d4d721096af65
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-05-12 21:36:40 -0400

    bug fix

--- alert/Alert.jsx
+++ alert/Alert.jsx
@@ -1,19 +1,24 @@
 import { useState, useEffect } from 'react';
 import './styles/Alert.css';
 
-const Alert = ({status, message, setMessage}) => {
+const Alert = ({status, message, classList, bodyStyle}) => {
+    const [isOpen, setIsOpen] = useState(true);
 
     useEffect(() => {
+        setIsOpen(true);
         const timer = setTimeout(() => {
-            setMessage('');
+            setIsOpen(false);
         }, 3000);
 
         return () => clearTimeout(timer);
     }, [message]);
 
     return (
-        message && message.length > 0 && (
-            <div className={`alert alert-${status}`}>
+        message && isOpen && (
+            <div 
+                className={`alert alert-${status}${classList ? ` ${classList}` : ''}`}
+                style={bodyStyle}
+            >
                 {message}
             </div>
         )
