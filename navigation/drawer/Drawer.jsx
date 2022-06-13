1a7ea19f4b9377f8531e34df854a2e6d3c944596
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-06-13 18:41:26 -0400

    resize

--- navigation/drawer/Drawer.jsx
+++ navigation/drawer/Drawer.jsx
@@ -1,7 +1,22 @@
+import { useEffect } from 'react'
 import { Link } from 'react-router-dom'
+import { closeIcon } from '../../../constants/icons'
 import './styles/Drawer.css'
 
 const Drawer = ({children, side, icon, label, secondary, isOpen, setIsOpen}) => {
+
+    const handleResize = () => {
+        setIsOpen(window.innerWidth > 768)
+    }
+
+    useEffect(() => {
+        window.addEventListener('resize', handleResize)
+
+        return () => {
+            window.removeEventListener('resize', handleResize)
+        }
+    }, [])
+
     return (
         <div
             className={`drawer${side ? ` drawer-${side}`: ' drawer-left'}${isOpen ? ' drawer-open' : ' drawer-closed'}`}
@@ -9,7 +24,13 @@ const Drawer = ({children, side, icon, label, secondary, isOpen, setIsOpen}) => 
             <div>
                 {(icon || label) &&
                     <div className="drawer-header">
-                        {icon ? <Link to='/' className="drawer-header-icon">{icon}</Link> : null}
+                        {window.innerWidth < 768 ? 
+                            <div className="drawer-header-icon" onClick={() => setIsOpen(false)}>
+                                {closeIcon}
+                            </div>
+                        :
+                            icon ? <Link to='/' className="drawer-header-icon">{icon}</Link> : null
+                        }
                         {label ? <div className="drawer-header-label">
                             {label} 
                             <div className="drawer-header-secondary">
