967b458276fbb17cf57c913d239338130dc5d40e
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-06-15 19:43:14 -0400

    util

--- inputs/button/Button.jsx
+++ inputs/button/Button.jsx
@@ -10,10 +10,31 @@ const Button = ({
     color,
     disabled,
     loading,
-    className
+    className,
+    to,
+    target
 }) => {
 
     return (
+        to ? 
+        <a
+            href={to}
+            target={target || '_blank'}
+            className={`btn${variant ? ` btn-${variant}` : ''}${size ? ` btn-${size}` : ''}${color ? ` btn-${color}` : ''}${disabled ? ' btn-disabled' : ''}${loading ? ' btn-loading' : ''}${className ? ` ${className}` : ''}`}
+            onClick={onClick ? onClick : null}
+            disabled={disabled}
+        >
+            { loading ? 
+                'Loading...'
+            :
+                <>
+                    {startIcon && <span className="btn-start-icon">{startIcon}</span>}
+                    {children}
+                    {endIcon && <span className="btn-end-icon">{endIcon}</span>}
+                </>
+            }
+        </a>
+        :
         <button
             className={`btn${variant ? ` btn-${variant}` : ''}${size ? ` btn-${size}` : ''}${color ? ` btn-${color}` : ''}${disabled ? ' btn-disabled' : ''}${loading ? ' btn-loading' : ''}${className ? ` ${className}` : ''}`}
             onClick={onClick ? onClick : null}
@@ -28,7 +49,6 @@ const Button = ({
                     {endIcon && <span className="btn-end-icon">{endIcon}</span>}
                 </>
             }
-
         </button>
     )
 }
