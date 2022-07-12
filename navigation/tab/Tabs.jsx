9d4b786dd4e6a7cd76bb5513dee75a03336d8eb7
Bohdan Khvorostovskyi <pevbog@gmail.com>
2022-07-12 19:38:59 -0400

    tabs

---
+++ navigation/tab/Tabs.jsx
@@ -0,0 +1,67 @@
+import { useRef, useState, useEffect } from 'react';
+import './styles/Tabs.css';
+
+const Tabs = ({items, active, onChange}) => {
+    const activeRef = useRef(null);
+    const indicatorRef = useRef(null);
+    const [indicatorWidth, setIndicatorWidth] = useState(0);
+    const [indicatorLeft, setIndicatorLeft] = useState(0);
+
+
+    useEffect(() => {
+        const activeTab = activeRef.current;
+        const indicator = indicatorRef.current;
+        if (indicator && activeTab) {
+            const activeTabWidth = activeTab.offsetWidth;
+            const activeTabLeft = activeTab.offsetLeft;
+            setIndicatorWidth(activeTabWidth);
+            setIndicatorLeft(activeTabLeft);
+        }
+    }, [active]);
+
+    return (
+        <div className="tabs">
+            <div className="tabs-container">
+                {items.map((item, index) => (
+                    <div
+                        key={index}
+                        className={`tab${active === index ? ' active': ''}`}
+                        onClick={() => onChange(index)}
+                        ref={active === index ? activeRef : null}
+                    >
+                        {item.icon && <span className="tab-icon">{item.icon}</span>}
+                        <span className="tab-label">{item.label}</span>
+                    </div>
+                ))}
+            </div>
+            <span className="tabs-indicator" ref={indicatorRef}
+                style={{
+                    width: indicatorWidth,
+                    left: indicatorLeft,
+                }}
+            />
+        </div>
+    )
+}
+
+export default Tabs
+
+// !Example of usage:
+{/* <Tabs
+    onChange={setActiveTab}
+    active={activeTab}
+    items={[
+        {
+            label: 'Home',
+            icon: homeIcon,
+        },
+        {
+            label: 'Grid',
+            icon: gridIcon,
+        },
+        {
+            label: 'Arrow',
+            icon: arrowIcon,
+        },
+    ]}
+/> */}
